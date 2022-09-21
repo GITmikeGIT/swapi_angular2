1fb18fb0847353e0218115554bb918c6d103e835
Mike <michalzajczyk@gmail.com>
2022-09-21 09:42:10 +0200

    Handle Click event type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { HttpService } from './services/http.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { TAG_NAME } from "./helpers/constants";
+import { HttpService } from "./services/http.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   constructor(private http: HttpService) {}
   swapi$: Observable<any>;
-  status: string = 'idle';
+  status: string = "idle";
   loading: boolean = true;
 
-  defaultUrl: string = 'https://swapi.dev/api';
+  defaultUrl: string = "https://swapi.dev/api";
   apiUrl: string = this.defaultUrl;
 
   ngOnInit() {
-    this.callApi(this.apiUrl, 'OnInit');
+    this.callApi(this.apiUrl, "OnInit");
   }
 
   callApi(url: string, source?: string): void {
-    console.log(url);
     this.setStatus(`${source} => call`);
     this.swapi$ = this.http.get(url).pipe(
       tap({
@@ -41,15 +41,13 @@ export class AppComponent implements OnInit {
     this.status = message;
   }
 
-  handleClick(event): void {
+  handleClick(event: PointerEvent): void {
     event.preventDefault();
     event.stopPropagation();
-    if (
-      'tagName' in event.target &&
-      event.target.tagName.toLowerCase() === 'a'
-    ) {
-      this.apiUrl = event.target.innerHTML;
-      this.callApi(this.apiUrl, 'handleClick');
+    const target = event.target as HTMLElement;
+    if (TAG_NAME in target && target[TAG_NAME].toLowerCase() === "a") {
+      this.apiUrl = target.innerHTML;
+      this.callApi(this.apiUrl, "handleClick");
     }
   }
 }
